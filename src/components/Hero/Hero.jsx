f241343fa6f7c1b8cb494f3b0edca38ff25362d1
Santiago Angel <santiago.angel@icaruslived.com>
2023-02-12 15:04:51 -0500

    more flash removal

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -36,15 +36,15 @@ const Header = () => {
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              {cta ? (
+          {cta ? (
+            <p className="hero-cta">
+              <span className="cta-btn cta-btn--hero">
                 <Link to="about" smooth duration={1000}>
                   {cta}
                 </Link>
-              ) : null}
-            </span>
-          </p>
+              </span>
+            </p>
+          ) : null}
         </Fade>
       </Container>
     </section>
